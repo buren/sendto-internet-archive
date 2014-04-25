d4aaae7c08081d913753acbfc79f96c26036ba51
Jacob Burenstam <burenstam@gmail.com>
2014-04-25 14:13:43 +0200

    fixes

--- archive.js
+++ archive.js
@@ -1,5 +1,3 @@
-'use strict';
-
 var fs        = require('fs'),
     system    = require('system');
 
@@ -7,11 +5,13 @@ var urlSource = '',
     domain    = '',
     callCount = 0;
 
+// sys args
 if (system.args.length === 1) {
-  console.log('Usage: arhive.js <some domain>');
+  console.log('Usage: arhive.js <some URL>');
   console.log('Usage: arhive.js <some file> file');
+  console.log('Usage: arhive.js <some domain> sitemap');
   phantom.exit();
-}else {
+} else {
   domain = system.args[1];
 }
 
@@ -24,37 +24,42 @@ if (urlSource !== 'file' && domain.indexOf('http://') === -1 && domain.indexOf('
   console.log('was: ' + domain);
   phantom.exit();
 }
+// end sys args
 
-var parseXml = function(xmlString) {
+var parseXml = function (xmlString) {
   return (new window.DOMParser()).parseFromString(xmlString, "text/xml");
 };
 
 var urlsFromSitemap = function (content) {
-  var xmlDoc = parseXml(content);
-  var locs = xmlDoc.getElementsByTagName('loc');
-  var urls = [];
-  for (var i = 0; i < locs.length; i++) {
-    var url = locs[i].childNodes[0].nodeValue;
+  var xmlDoc = parseXml(content),
+      locs = xmlDoc.getElementsByTagName('loc'),
+      urls = [],
+      i    = null,
+      url  = null ;
+
+  for (i = 0; i < locs.length; i += 1) {
+    url = locs[i].childNodes[0].nodeValue;
     urls.push(url);
-  };
+  }
   return urls;
 };
 
 // Remove query string from each URL and return unique URLs
 var filterUniq = function(urls) {
-  var urlsObj = {};
-  for (var i = 0; i < urls.length; i++) {
-    var url = urls[i];
-    var paramIndex = urls[i].indexOf('?');
-    if (paramIndex !== -1) {
-      url = urls[i].substr(0, paramIndex);
-    }
+  var urlsObj      = {},
+      i            = null,
+      url          = null,
+      paramIndex   = null,
+      filteredUrls = [],
+      filteredURL  = null;
+
+  for (i = 0; i < urls.length; i += 1) {
+    url = urls[i].split('?')[0]; // Remove all URL params
     urlsObj[url] = url;
   }
-  var filteredUrls = [];
-  for (var url in urlsObj) {
-    if (urlsObj.hasOwnProperty(url)) {
-      filteredUrls.push(url);
+  for (filteredURL in urlsObj) {
+    if (urlsObj.hasOwnProperty(filteredURL)) {
+      filteredUrls.push(filteredURL);
     }
   }
   return filteredUrls;
@@ -62,23 +67,16 @@ var filterUniq = function(urls) {
 
 var archivePage = function(url, maxCalls) {
   var page = require('webpage').create();
-  page.open('https://web.archive.org/save/' + url, function(status) {
-    callCount++;
+  page.open('https://web.archive.org/save/' + url, function() {
+    callCount += 1;
     if (callCount === maxCalls){
       console.log('Sent ' + callCount + ' URLs to the Internet Archive');
       phantom.exit();
     } else {
-      console.log('Sent ' + callCount + ' of ' + maxCalls);
+      console.log('Sent ' + callCount + ' of ' + maxCalls + '     url: ' + url);
     }
     page.close();
   });
-};
-
-var archivePages = function(archiveUrls) {
-  var urls = filterUniq(archiveUrls);
-  for (var i = 0; i < urls.length; i++) {
-    archivePage(urls[i], urls.length)
-  };
 };
 
 var fileLines = function(filePath) {
@@ -86,7 +84,8 @@ var fileLines = function(filePath) {
       f       = null,
       res     = [],
       lines   = null,
-      eol     = system.os.name == 'windows' ? "\r\n" : "\n";
+      i       = 0,
+      eol     = system.os.name === 'windows' ? "\r\n" : "\n";
 
   try {
     f       = fs.open(filePath, "r");
@@ -100,7 +99,7 @@ var fileLines = function(filePath) {
 
   if (content) {
     lines = content.split(eol);
-    for (var i = 0, len = lines.length; i < len; i++) {
+    for (i = 0; i < lines.length; i += 1) {
       if (lines[i].length > 0) {
         res.push(lines[i]);
       }
@@ -109,20 +108,36 @@ var fileLines = function(filePath) {
   return res;
 };
 
-if (urlSource == 'sitemap') {
-  var sitemap = require('webpage').create();
-  sitemap.open(domain + "/sitemap.xml", function(status) {
-    if(status !== "success") {
-      console.log("Unable to open sitemap.");
-      console.log("Exiting.");
-      phantom.exit();
-    } else {
-      archivePages(urlsFromSitemap(sitemap.content));
+var run = function() {
+  var urls = null;
+  if (urlSource === 'sitemap') {
+    console.log('Getting /sitemap.xml');
+    var sitemap = require('webpage').create();
+    sitemap.open(domain + "/sitemap.xml", function(status) {
+      if(status !== "success") {
+        console.log("Unable to open sitemap.");
+        console.log("Exiting.");
+        phantom.exit();
+      } else {
+        urls = filterUniq(urlsFromSitemap(sitemap.content));
+        for (i = 0; i < urls.length; i += 1) {
+          archivePage(urls[i], urls.length);
+        }
+      }
+    });
+  } else if (urlSource === 'file') {
+    console.log('Archiving all URLS in file');
+    urls = filterUniq(fileLines(domain));
+    for (i = 0; i < urls.length; i += 1) {
+      archivePage(urls[i], urls.length);
     }
-  });
-} else if (urlSource == 'file') {
-  var urls = fileLines(domain);
-  archivePages(urls);
-} else {
-  archivePages([domain]);
-}
+  } else {
+    console.log('Submitting URL');
+    urls = [domain];
+    for (i = 0; i < urls.length; i += 1) {
+      archivePage(urls[i], urls.length);
+    }
+  }
+};
+
+run();
